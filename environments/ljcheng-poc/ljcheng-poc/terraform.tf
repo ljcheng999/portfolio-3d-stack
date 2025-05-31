517f6738ddebae07bdb44955a36cfbe8e14ef2d3
ljcheng-tech <johnny.cheng919@gmail.com>
2025-05-30 20:29:41 -0600

    ready to use pipeline to create cf and s3

---
+++ environments/ljcheng-poc/ljcheng-poc/terraform.tf
@@ -0,0 +1,11 @@
+
+
+domain_name = "kubesources.com"
+subdomain   = "ljcheng-poc"
+
+cloudfront_staging            = false
+cloudfront_waf_default_action = "allow"
+additional_tags               = {}
+
+
+# assume_role_str   = ""
